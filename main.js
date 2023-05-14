15cbdeb4c36f0ceec1649dc8449a090c59478176
lui779 <garcesluis9@gmail.com>
2023-05-13 20:56:25 -0400

    TareaS8

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 
 for (let i = 10; i >= 0; i--) {
     console.log(i);
+    if (i === 0){
+        console.log('¡Despegamos!');
+    }
 }
 
-console.log('¡Despegamos!')
+/*  recibir un número y mostrar en
+la consola una cuenta regresiva desde ese
+número hasta el cero. Además, cuando la cuenta
+llegue a cero, en la consola se mostrará el
+mensaje ¡Despegamos!
+Por ejemplo, si a esta función le paso el número
+10  tarea 6 S8 entregada el 13/05/23 */
